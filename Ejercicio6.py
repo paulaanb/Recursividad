08bfcfac020686919ae3dcf607115a134cc8abda
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-02-27 20:40:42 +0100

    valores de los remplazadores

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -10,3 +10,16 @@ class verificacion:
     else:
       print('La palabra no es palindroma')
   
+resultado = verificacion(palabra)
+print(resultado.palabra_palindroma)
+
+#Valores de los remplazadores
+p= palabra
+x = ''
+remplazadores = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+    )
